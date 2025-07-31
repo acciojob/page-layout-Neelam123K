d260b72c70b23640299d4db23d1d7461856ccb90
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-07-31 12:52:01 +0530

    work

---
+++ src/components/PageLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function PageLayout({ header , children , footer }) {
+  return (
+    <div>
+      <header>{header}</header>
+      <main>{children}</main>
+      <footer>{footer}</footer>
+
+    </div>
+  );
+}
+
+export default PageLayout;
